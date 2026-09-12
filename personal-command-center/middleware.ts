cdf0c33894b6506cb9b5c4a844a37b130704bde8
punwa <wattanakraimetpannarat@gmail.com>
2026-09-12 17:00:32 +0700

    Remove duplicated folder

--- personal-command-center/middleware.ts
+++
@@ -1,8 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  // ป้องกันทุกหน้า ยกเว้นหน้า login, api/auth, และไฟล์ static
-  matcher: [
-    "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
-  ],
-};
